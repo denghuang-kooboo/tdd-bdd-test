7f74dd3e186430cd7d2d597c5f844f11012c71e8
tenghui <1246623393@qq.com>
2019-01-17 18:30:13 +0800

    ~add ut

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,64 +1,70 @@
 import { shallowMount } from '@vue/test-utils'
 import Sort from '@/components/sort.vue'
-import { sort } from 'semver';
 import _ from 'lodash'
 
 describe('Sort.vue', () => {
 
+  const sortOptions = [
+    {
+      label: '时间升序',
+      sortIndex: '1',
+      order: 'asc',
+      sortBy: 'time'
+    },
+    {
+      label: '时间降序',
+      sortIndex: '2',
+      order: 'desc',
+      sortBy: 'time'
+    },
+    {
+      label: '价格升序',
+      sortIndex: '3',
+      order: 'asc',
+      sortBy: 'price'
+    }
+  ]
+
   function createWrapper(options) {
     return shallowMount(Sort, _.merge({
       propsData: {
-        sortOptions: []
+        sortOptions: sortOptions,
+        selectedSort: {}
       }
     }, options || {}))
   }
 
-  it('renders props.msg when passed', () => {
-    const wrapper = shallowMount(Sort)
-    console.log(wrapper.html())
-  })
-
   it('should default sortTitle', () => {
-    const wrapper = shallowMount(Sort)
+    const wrapper = createWrapper()
+
     expect(wrapper.vm.sortTitle).toBe('default sort title')
   })
 
   it('sortTitle should be same as seteed', () => {
-    const wrapper = shallowMount(Sort)
-     wrapper.setProps({sortTitle: 'sort title'})
+    const wrapper = createWrapper()
+
+    wrapper.setProps({ sortTitle: 'sort title' })
     expect(wrapper.vm.sortTitle).toBe('sort title')
   })
 
-  it('should emitted the selected option', () => {
+  it('time asc radio should selected when  selectedSort is time asc', () => {
     const wrapper = createWrapper({
       propsData: {
-        sortTitle: 'sort title',
-        sortOptions: [
-          {
-            label: '时间升序',
-            sortIndex: '1',
-            order: 'asc',
-            sortBy: 'time'
-          },
-          {
-            label: '时间降序',
-            sortIndex: '2',
-            order: 'desc',
-            sortBy: 'time'
-          },
-          {
-            label: '价格升序',
-            sortIndex: '3',
-            order: 'asc',
-            sortBy: 'price'
-          }
-        ],
         selectedSort: {
-          order: 'desc',
+          order: 'asc',
           sortBy: 'time'
         }
       }
     })
+
+    let thirdRadio = wrapper.find('[value="time:asc"]')
+
+    // 怎么验证默认选中
+    console.log(thirdRadio.vm)
+  })
+
+  it('should emitted the selected option', () => {
+    const wrapper = createWrapper()
 
     let thirdRadio = wrapper.find('[value="price:asc"]')
     thirdRadio.trigger('click')
@@ -68,36 +74,8 @@ describe('Sort.vue', () => {
     expect(emitted[0][0].sortBy).toBe('price')
   })
 
-  it('should emitted the selected option', () => {
-    const wrapper = createWrapper({
-      propsData: {
-        sortTitle: 'sort title',
-        sortOptions: [
-          {
-            label: '时间升序',
-            sortIndex: '1',
-            order: 'asc',
-            sortBy: 'time'
-          },
-          {
-            label: '时间降序',
-            sortIndex: '2',
-            order: 'desc',
-            sortBy: 'time'
-          },
-          {
-            label: '价格升序',
-            sortIndex: '3',
-            order: 'asc',
-            sortBy: 'price'
-          }
-        ],
-        selectedSort: {
-          order: 'desc',
-          sortBy: 'time'
-        }
-      }
-    })
+  it('should display all sortOption', () => {
+    const wrapper = createWrapper()
 
     expect(wrapper.findAll('[type="radio"]').length).toBe(3)
   })
