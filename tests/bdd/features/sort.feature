5b90e51e1d60d378735d225f8960b85feaba0792
denghuang@kooboo.cn <denghuang@kooboo.cn>
2019-01-17 21:26:46 +0800

    bdd case

--- tests/bdd/features/sort.feature
+++ tests/bdd/features/sort.feature
@@ -1,6 +1,33 @@
-Feature: Sort function
-  In order to show result page by order, we need to provide order function for users to sort the result page
+Feature: 卡片列表页面 添加排序功能
+  1. 页面加载后要显示排序列表
+  1) 标题： “排序”
+  2) 列表： 时间升序、时间降序、价格升序
+  2. 点击排序选项，卡片会做相应的排序
 
-Scenario: show sort sidebar in result page
-  Given I open home page
-  Then "detault sort title" should show in result page
+  Scenario: 显示“排序”标题
+    Given 在卡片列表页面
+    When 页面加载完成
+    Then 标题"排序"要正常显示
+
+  Scenario: 默认排序
+    Given 在卡片列表页面
+    When 页面加载完成
+    Then "时间降序"选项被选中
+    And 卡片默认按照"时间降序"排列
+
+  Scenario Outline: 显示排序列表
+    Given 在卡片列表页面
+    When 页面加载完成
+    Then 可以看到<sortOption>排序列表选项
+
+  Scenario Outline: 用户点击排序选项
+    Given 在卡片列表页面
+    When 用户点击<sortOption>
+    Then <sortOption>被选中
+    And 卡片按照<sortOption>排列
+
+    Examples:
+      | sortOption |
+      | 时间升序       |
+      | 时间降序       |
+      | 价格升序       |
--- tests/bdd/features/step-definitions/sort.js
+++ tests/bdd/features/step-definitions/sort.js
@@ -3,11 +3,69 @@ const { Given, Then, When } = require('cucumber');
 
 const { url } = require('../../conf/default.conf').test_settings.default.globals;
 
-Given('I open home page', function () {
+Given('在卡片列表页面', function () {
   return client.url(url);
 });
 
-Then('{string} should show in result page', function (title) {
-  console.log(title)
+When('页面加载完成', function () {
   return client.assert.visible('.sort-title')
+});
+
+Then('标题{string}要正常显示', function (string) {
+  return client.expect.element('.sort-title').text.to.equal(string)
+});
+
+Then('{string}选项被选中', function (string) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Then('卡片默认按照{string}排列', function (string) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+When('用户点击时间升序', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Then('时间升序被选中', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Then('卡片按照时间升序排列', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+When('用户点击时间降序', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Then('时间降序被选中', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Then('卡片按照时间降序排列', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+When('用户点击价格升序', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Then('价格升序被选中', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Then('卡片按照价格升序排列', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
 });
