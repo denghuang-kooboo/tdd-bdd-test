f91cbd297395097384c2145c9fa039d11858c687
tenghui <1246623393@qq.com>
2019-01-17 17:26:11 +0800

    ~formatter

--- src/components/sort.vue
+++ src/components/sort.vue
@@ -1,15 +1,11 @@
 <template>
   <div>
-    <p class="sort-title"> {{ sortTitle }} </p>
-    <div class="">
-      <div
-        class=""
-        v-for="(item, index) in sortedOptions"
-        :key="index"
-      >
+    <p class="sort-title">{{ sortTitle }}</p>
+    <div class>
+      <div class v-for="(item, index) in sortedOptions" :key="index">
         <input
           :value="item.value"
-          :checked='isChecked(item.value)'
+          :checked="isChecked(item.value)"
           @input="sortHandler"
           type="radio"
         >
@@ -20,19 +16,19 @@
 </template>
 
 <script>
-import _ from "lodash";
+import _ from 'lodash'
 export default {
-  name: "sort",
+  name: 'sort',
   data() {
     return {
-      sortValue: ""
-    };
+      sortValue: ''
+    }
   },
   props: {
     sortTitle: {
       type: String,
       default() {
-        return "default sort title";
+        return 'default sort title'
       }
     },
     sortOptions: {
@@ -46,53 +42,53 @@ export default {
   },
   computed: {
     sortedOptions() {
-      let sortedOptions = _.sortBy(this.sortOptions, "sortIndex");
+      let sortedOptions = _.sortBy(this.sortOptions, 'sortIndex')
       return _.map(sortedOptions, o => {
         return {
           label: o.label,
-          value: o.sortBy + ":" + o.order
-        };
-      });
+          value: o.sortBy + ':' + o.order
+        }
+      })
     }
   },
   methods: {
     sortHandler(e) {
       let value = e.target.value
       this.sortValue = value
-      let sortObj = this.convertValueToSortObj(value);
+      let sortObj = this.convertValueToSortObj(value)
       if (!sortObj) {
-        return;
+        return
       }
-      this.$emit("sort", sortObj);
+      this.$emit('sort', sortObj)
     },
     convertValueToSortObj(value) {
       if (!value) {
-        return null;
+        return null
       }
-      let arr = value.split(":");
+      let arr = value.split(':')
       if (arr.length !== 2) {
-        return null;
+        return null
       }
 
       return {
         sortBy: arr[0],
         order: arr[1]
-      };
+      }
     },
     isChecked(value) {
-      return value === this.sortValue;
+      return value === this.sortValue
     }
   },
   watch: {
     selectedSort: {
       handler(val) {
         if (!val) {
-          return;
+          return
         }
-        this.sortValue = val.sortBy + ":" + val.order;
+        this.sortValue = val.sortBy + ':' + val.order
       },
       immediate: true
     }
   }
-};
+}
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,87 +1,80 @@
 <template>
   <div class="home">
-    <tdd-sort
-      :sort-options="sortOptions"
-      :selected-sort="sortObj"
-      @sort="sorting"
-    ></tdd-sort>
+    <tdd-sort :sort-options="sortOptions" :selected-sort="sortObj" @sort="sorting"></tdd-sort>
     <hr style="margin: 20px 0px">
-    <div
-      v-for="item in planList"
-      :key="item.id"
-    >
-      <div> {{ item.name }}</div>
-      <div> {{ item.price }}</div>
-      <div> {{ item.time.format('YYYY-MM-DD') }}</div>
+    <div v-for="item in planList" :key="item.id">
+      <div>{{ item.name }}</div>
+      <div>{{ item.price }}</div>
+      <div>{{ item.time.format('YYYY-MM-DD') }}</div>
       <hr>
     </div>
   </div>
 </template>
 
 <script>
-import moment from "moment";
-import TddSort from "@/components/sort";
-import _ from "lodash";
+import moment from 'moment'
+import TddSort from '@/components/sort'
+import _ from 'lodash'
 export default {
-  name: "home",
+  name: 'home',
   components: {
     TddSort
   },
-  data() {
+  data () {
     return {
       planList: [
         {
-          id: "p-id-1",
-          name: "a-p-name",
+          id: 'p-id-1',
+          name: 'a-p-name',
           price: 2000,
-          time: moment("2010-01-05")
+          time: moment('2010-01-05')
         },
         {
-          id: "p-id-3",
-          name: "a-p-name",
+          id: 'p-id-3',
+          name: 'a-p-name',
           price: 1000,
-          time: moment("2009-01-05")
+          time: moment('2009-01-05')
         },
         {
-          id: "p-id-2",
-          name: "c-p-name",
+          id: 'p-id-2',
+          name: 'c-p-name',
           price: 1000,
-          time: moment("2003-01-05")
+          time: moment('2003-01-05')
         }
       ],
       sortObj: {
-        sortBy: "time",
-        order: "desc"
+        sortBy: 'time',
+        order: 'desc'
       },
       sortOptions: [
-          {
-            label: "时间升序",
-            sortIndex: "1",
-            order: "asc",
-            sortBy: "time"
-          },
-          {
-            label: "时间降序",
-            sortIndex: "2",
-            order: "desc",
-            sortBy: "time"
-          },
-          {
-            label: "价格升序",
-            sortIndex: "3",
-            order: "asc",
-            sortBy: "price"
-          }
-        ]
-    };
+        {
+          label: '时间升序',
+          sortIndex: '1',
+          order: 'asc',
+          sortBy: 'time'
+        },
+        {
+          label: '时间降序',
+          sortIndex: '2',
+          order: 'desc',
+          sortBy: 'time'
+        },
+        {
+          label: '价格升序',
+          sortIndex: '3',
+          order: 'asc',
+          sortBy: 'price'
+        }
+      ]
+    }
   },
   methods: {
-    sorting(value) {
-      this.sortObj = _.clone(value);
-      let sortBy = value.sortBy;
-      let order = value.order;
-      this.planList = _.orderBy(this.planList, [sortBy], [order]);
+    sorting (value) {
+      this.sortObj = _.clone(value)
+      let sortBy = value.sortBy
+      let order = value.order
+      this.planList = _.orderBy(this.planList, [sortBy], [order])
     }
   }
-};
+}
 </script>
